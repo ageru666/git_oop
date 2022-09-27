1ec8a31d4f2ef02a321ad0f333df8303514ace04
Илья Волошин <ageru2003@gmail.com>
2022-09-27 21:00:12 +0300

    Created main menu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Lab1
+﻿namespace Lab1_Voloshyn
 {
-    internal class Program
+
+    static class MainMenu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Choose one of the options below.");
+            Console.WriteLine("1. Geometry");
+            Console.WriteLine("2. Sorting");
+            string input = Console.ReadLine();
+            switch (input) 
+            {
+                case "1":
+                    //Geometry.AnyAngleInput(); 
+                    break;
+                case "2":
+                    //Sorting.SortingMenu(); 
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Try again.\n");
+                    break;
+            }
+            Menu();
         }
     }
 }
