d4c17cba5ac141c9166dd3473adcdcba1d554765
Илья Волошин <ageru2003@gmail.com>
2022-10-01 20:04:06 +0300

    Implementation of geometry(Fiveangle)

---
+++ Fiveangle.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab1_Voloshin.Geometry
+{
+    internal class Fiveangle
+    {
+        uint a, b, c, d, e;
+        uint[] sides;
+        public uint A { get => a; }
+        public uint B { get => b; }
+        public uint C { get => c; }
+        public uint D { get => d; }
+        public uint E { get => e; }
+
+        public Fiveangle(uint a, uint b, uint c, uint d, uint e)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+            sides = new uint[] { a,b,c,d,e };
+        }
+
+        public void CalculateFiveangle()
+        {
+            if (sides[0] == sides[1] && sides[0] == sides[2] && sides[0] == sides[3] && sides[0] == sides[4])
+            {
+                Console.WriteLine($"Your figure is an right fiveangle.");
+                double S = Math.Sqrt(((Math.Pow(sides[0], 2) + (sides[0] * 2) * Math.Sqrt(sides[0])) / 4) * Math.Pow(sides[0], 2));
+                Console.WriteLine($"Area of your fiveangle is: {S}  square cm");
+            }
+            else
+                Console.WriteLine($"Your figure is not right fiveangle. (Not all sides are the same)");
+        }
+    }
+}
